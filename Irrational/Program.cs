47fab2c7e992939e618f3886ef05d66a3b3e44fc
Epox277 <kpbillia219@gmail.com>
2017-05-11 17:53:36 +0300

    Butify Program.cs

--- Irrational/Program.cs
+++ Irrational/Program.cs
@@ -10,31 +10,13 @@ namespace IrrationalSpace
 {
     class Program
     {
-       
-       
-       
-        public static bool autorotate = false;
-        private static List<Star> stars = new List<Star>();
-        private static Random generator = new Random(Environment.TickCount);
-        private static List<SceneObject> objectsOnScene = new List<SceneObject>();
-
-      
-        public static float lightStr = 1f;
-        public static bool fullscreen = false;
-        public static bool alphaBlending = false;
-        public static float alphaStr =2f;
-        
-
-        static void Main(string[] args)
+		static void Main(string[] args)
         {
 			using (ApplicationWindow window = new ApplicationWindow())
 			{
+				//Framerate as parameter
 				window.Run(60);
 			}
         }
-
-
-
-
- 
-        }}
+	}
+}
