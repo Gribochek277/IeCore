2ab098c134d0a8ac3be5096270b553f3acf442b4
DESKTOP-AQ0O6U5\kpbil <kpbillia219@gmail.com>
2017-11-09 02:22:11 +0200

    Attributes for shaders moved to OnLoad()

--- Irrational/Core/Renderer/OpenglRenderer.cs
+++ Irrational/Core/Renderer/OpenglRenderer.cs
@@ -42,45 +42,53 @@ namespace Irrational.Core.Renderer
             GL.GenBuffers(1, out ibo_elements);
 
             // Load shaders from file
-            shaders.Add("default", new ShaderProg("vs.glsl", "fs.glsl", true));
-            shaders.Add("textured", new ShaderProg("vs_tex.glsl", "fs_tex.glsl", true));
-            shaders.Add("normal", new ShaderProg("vs_norm.glsl", "fs_norm.glsl", true));
+            //shaders.Add("default", new ShaderProg("vs.glsl", "fs.glsl", true));
+            //shaders.Add("textured", new ShaderProg("vs_tex.glsl", "fs_tex.glsl", true));
+            //shaders.Add("normal", new ShaderProg("vs_norm.glsl", "fs_norm.glsl", true));
 
             //activeShader = "normal";
 
             light = new SceneObject() { ModelMesh = new Mesh() { Position = new Vector3(1, 1, 1) } };
 
-            for(int i = 0; i < 4; i++) { 
+            for (int i = 0; i < 2; i++) { 
             SceneObject sceneObject = new SceneObject() { MaterialSource = "Resources/knight3.mtl" };
             sceneObject.OnLoad();
-			sceneObject.shader = new ShaderProg("vs_tex.glsl", "fs_tex.glsl", true);
-            materials = sceneObject.materials;
-            textures = sceneObject.textures;
+               
+                sceneObject.shader = new ShaderProg("vs_tex.glsl", "fs_tex.glsl", true);
 
-            // Create our objects
-            WavefrontModelLoader modelLoader = new WavefrontModelLoader();
+                materials[sceneObject.materials.FirstOrDefault().Key] = sceneObject.materials.FirstOrDefault().Value;
+                textures[sceneObject.textures.FirstOrDefault().Key] = sceneObject.textures.FirstOrDefault().Value;
+
+                // Create our objects
+                WavefrontModelLoader modelLoader = new WavefrontModelLoader();
             sceneObject.ModelMesh = modelLoader.LoadFromFile("Resources/knight3.obj1");
-            //sceneObject.ModelMesh.CalculateNormals();
+            sceneObject.ModelMesh.CalculateNormals();
             sceneObject.Position += new Vector3(0+(i*3), 0.0f, -10);
             sceneObject.ModelMesh.TextureID = textures[materials["Knight"].DiffuseMap];
-            sceneObject.Scale = new Vector3(1f, 1f, 1f);
+         //   sceneObject.Scale = new Vector3(1f, 1f, 1f);
             objects.Add(sceneObject);
 
+           }
 
- sceneObject = new SceneObject() { MaterialSource = "Resources/knight3.mtl" };
-sceneObject.OnLoad();
-			sceneObject.shader = new ShaderProg("vs_norm.glsl", "fs_norm.glsl", true);
-            materials = sceneObject.materials;
-            textures = sceneObject.textures;
+            for (int i = 0; i < 2; i++)
+            {
+                SceneObject sceneObject = new SceneObject() { MaterialSource = "Resources/knight3.mtl" };
+                sceneObject.OnLoad();
 
-            // Create our objects
-             modelLoader = new WavefrontModelLoader();
-sceneObject.ModelMesh = modelLoader.LoadFromFile("Resources/knight3.obj1");
-            //sceneObject.ModelMesh.CalculateNormals();
-            sceneObject.Position += new Vector3(0+(i*3*4), 0.0f, -10);
-            sceneObject.ModelMesh.TextureID = textures[materials["Knight"].DiffuseMap];
-            sceneObject.Scale = new Vector3(1f, 1f, 1f);
-objects.Add(sceneObject);
+                sceneObject.shader = new ShaderProg("vs_norm.glsl", "fs_norm.glsl", true);
+
+                materials[sceneObject.materials.FirstOrDefault().Key] = sceneObject.materials.FirstOrDefault().Value;
+                textures[sceneObject.textures.FirstOrDefault().Key] = sceneObject.textures.FirstOrDefault().Value;
+
+                // Create our objects
+                WavefrontModelLoader modelLoader = new WavefrontModelLoader();
+                sceneObject.ModelMesh = modelLoader.LoadFromFile("Resources/knight3.obj1");
+                 sceneObject.ModelMesh.CalculateNormals();
+                sceneObject.Position += new Vector3(0 + (i * 3), 0.0f, -10);
+                sceneObject.ModelMesh.TextureID = textures[materials["Knight"].DiffuseMap];
+                //   sceneObject.Scale = new Vector3(1f, 1f, 1f);
+                objects.Add(sceneObject);
+
             }
 
 
@@ -117,45 +125,47 @@ objects.Add(sceneObject);
 
             Console.WriteLine("Vertexies: " + vertdata.Length);
             Console.WriteLine("Triangles: " + vertdata.Length/3);
+
+            int indiceat = 0;
+            foreach (SceneObject v in objects)
+            {
+
+                GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vPosition"));
+
+                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length * Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+                GL.VertexAttribPointer(v.shader.GetAttribute("vPosition"), 3, VertexAttribPointerType.Float, false, 0, 0);
+
+                // Buffer vertex color if shader supports it
+                if (v.shader.GetAttribute("vColor") != -1)
+                {
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vColor"));
+                    GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(coldata.Length * Vector3.SizeInBytes), coldata, BufferUsageHint.StaticDraw);
+                    GL.VertexAttribPointer(v.shader.GetAttribute("vColor"), 3, VertexAttribPointerType.Float, true, 0, 0);
+                }
+
+
+                // Buffer texture coordinates if shader supports it
+                if (v.shader.GetAttribute("texcoord") != -1)
+                {
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("texcoord"));
+                    GL.BufferData<Vector2>(BufferTarget.ArrayBuffer, (IntPtr)(texcoorddata.Length * Vector2.SizeInBytes), texcoorddata, BufferUsageHint.StaticDraw);
+                    GL.VertexAttribPointer(v.shader.GetAttribute("texcoord"), 2, VertexAttribPointerType.Float, true, 0, 0);
+                }
+
+                if (v.shader.GetAttribute("vNormal") != -1)
+                {
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vNormal"));
+                    GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(normdata.Length * Vector3.SizeInBytes), normdata, BufferUsageHint.StaticDraw);
+                    GL.VertexAttribPointer(v.shader.GetAttribute("vNormal"), 3, VertexAttribPointerType.Float, true, 0, 0);
+                }
+                indiceat += v.ModelMesh.IndiceCount;
+
+            }
         }
 
         public void OnRendered()
         {
-
-			foreach (SceneObject v in objects)
-			{
-				GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vPosition"));
-
-            	GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length* Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
-				GL.VertexAttribPointer(v.shader.GetAttribute("vPosition"), 3, VertexAttribPointerType.Float, false, 0, 0);
-
-            // Buffer vertex color if shader supports it
-			if (v.shader.GetAttribute("vColor") != -1)
-            {
-				GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vColor"));
-                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(coldata.Length* Vector3.SizeInBytes), coldata, BufferUsageHint.StaticDraw);
-				GL.VertexAttribPointer(v.shader.GetAttribute("vColor"), 3, VertexAttribPointerType.Float, true, 0, 0);
-            }
-
-
-            // Buffer texture coordinates if shader supports it
-			if (v.shader.GetAttribute("texcoord") != -1)
-            {
-				GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("texcoord"));
-                GL.BufferData<Vector2>(BufferTarget.ArrayBuffer, (IntPtr)(texcoorddata.Length* Vector2.SizeInBytes), texcoorddata, BufferUsageHint.StaticDraw);
-				GL.VertexAttribPointer(v.shader.GetAttribute("texcoord"), 2, VertexAttribPointerType.Float, true, 0, 0);
-            }
-
-			if (v.shader.GetAttribute("vNormal") != -1)
-            {
-				GL.BindBuffer(BufferTarget.ArrayBuffer, v.shader.GetBuffer("vNormal"));
-                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(normdata.Length* Vector3.SizeInBytes), normdata, BufferUsageHint.StaticDraw);
-				GL.VertexAttribPointer(v.shader.GetAttribute("vNormal"), 3, VertexAttribPointerType.Float, true, 0, 0);
-            }
-
-
-
-			}
+          
             // Update object positions
             time += (float)this._gameWindow.RenderPeriod;
 
@@ -181,53 +191,55 @@ objects.Add(sceneObject);
             foreach(SceneObject v in objects)
             {
 				GL.UseProgram(v.shader.ProgramID);
-				v.shader.EnableVertexAttribArrays();     
+				v.shader.EnableVertexAttribArrays();
+
                 GL.BindTexture(TextureTarget.Texture2D, v.ModelMesh.TextureID);
-				GL.UniformMatrix4(v.shader.GetUniform("model"), false, ref v.ModelMesh.ModelMatrix);
+
+                GL.UniformMatrix4(v.shader.GetUniform("model"), false, ref v.ModelMesh.ModelMatrix);
                 Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(1.3f, _gameWindow.ClientSize.Width / (float)_gameWindow.ClientSize.Height, 1.0f, 40.0f);
-				GL.UniformMatrix4(v.shader.GetUniform("projection"), false, ref projection);
+                GL.UniformMatrix4(v.shader.GetUniform("projection"), false, ref projection);
                 Matrix4 view = cam.GetViewMatrix();
-				GL.UniformMatrix4(v.shader.GetUniform("view"), false, ref view);
-
-				if (v.shader.GetUniform("lightColor") != -1)
+                GL.UniformMatrix4(v.shader.GetUniform("view"), false, ref view);
+                if (v.shader.GetUniform("maintexture") != -1)
                 {
-					GL.Uniform3(v.shader.GetUniform("lightColor"), 1f, 1f, 1F);
+                    GL.Uniform1(v.shader.GetAttribute("maintexture"), v.ModelMesh.TextureID);
                 }
 
-				if (v.shader.GetUniform("ambientStr") != -1)
+                if (v.shader.GetUniform("lightColor") != -1)
                 {
-					GL.Uniform1(v.shader.GetUniform("ambientStr"), 0.3f);
+                    GL.Uniform3(v.shader.GetUniform("lightColor"), 1f, 1f, 1F);
                 }
 
-				if(v.shader.GetUniform("specStr") != -1)
+                if (v.shader.GetUniform("ambientStr") != -1)
+                {
+                    GL.Uniform1(v.shader.GetUniform("ambientStr"), 0.3f);
+                }
+
+                if (v.shader.GetUniform("specStr") != -1)
                 {
                     //TODO : find a way how to extract specular exponent from material. Additional refactoring is requiered
-					GL.Uniform1(v.shader.GetUniform("specStr"), 10f);
+                    GL.Uniform1(v.shader.GetUniform("specStr"), 10f);
                 }
 
-				if (v.shader.GetUniform("cameraPosition") != -1)
+                if (v.shader.GetUniform("cameraPosition") != -1)
                 {
-					GL.Uniform3(v.shader.GetUniform("cameraPosition"), cam.Position.X, cam.Position.Y, cam.Position.Z);
+                    GL.Uniform3(v.shader.GetUniform("cameraPosition"), cam.Position.X, cam.Position.Y, cam.Position.Z);
                 }
 
-				if (v.shader.GetUniform("lightPos") != -1)
+                if (v.shader.GetUniform("lightPos") != -1)
                 {
-					GL.Uniform3(v.shader.GetUniform("lightPos"), light.Position.X,light.Position.Y,light.Position.Z);
-                }
-
-				if (v.shader.GetUniform("maintexture") != -1)
-                {
-					GL.Uniform1(v.shader.GetAttribute("maintexture"), v.ModelMesh.TextureID);                    
+                    GL.Uniform3(v.shader.GetUniform("lightPos"), light.Position.X, light.Position.Y, light.Position.Z);
                 }
 
                 GL.DrawElements(BeginMode.Triangles, v.ModelMesh.IndiceCount, DrawElementsType.UnsignedInt, indiceat * sizeof(uint));
                 indiceat += v.ModelMesh.IndiceCount;
+          
 
-				v.shader.DisableVertexAttribArrays();
 
+                v.shader.DisableVertexAttribArrays();
             }
 
-           
+
             GL.Flush();
             _gameWindow.SwapBuffers();
         }
@@ -246,8 +258,8 @@ objects.Add(sceneObject);
         {
             for(int i=0;i<objects.Count;i++)
             {
-				objects[i].Position = new Vector3(-(objects.Count/2) + i + ((float)Math.Sin(time) * 2), -0.5f + (float)Math.Sin(time), -3.0f);
-                objects[i].Rotation = new Vector3(0.55f * time, 0.25f * time, 0);
+				objects[i].Position = new Vector3(-(objects.Count) + i*3.5f, 0, -5.0f);
+                objects[i].Rotation = new Vector3(0, 0.25f * time, 0);
             }
 
 
