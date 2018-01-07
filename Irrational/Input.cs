49f5e51aa6a167e60f46c7b48ab087b2d12fa83a
Sergey <kpbillia219@gmail.com>
2018-01-07 20:39:04 +0200

    Revert "Project moved to .net core."
    This reverts commit dd00001cc0dd08078b52bc90e6993971a92acab3.

---
+++ Irrational/Input.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OpenGLTUT
+{
+    public class Input
+    {
+        public Input()
+        {
+            
+        }
+    }
+}
---
+++ Irrational/Program.cs
@@ -0,0 +1,18 @@
+﻿using Irrational.Core.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Irrational
+{
+    class Program
+    {
+		static void Main(string[] args)
+        {
+            OpenTKWindow window = new OpenTKWindow();
+        }
+	}
+}
