0416b6f69084d7b0fdff99000309b129133a4975
Sergey Alpatov <kpbillia219@gmail.com>
2018-12-02 01:36:40 +0200

    Added sliders to be able rotate object in more efficient way

--- IrrationalEngineCore/IrrationalEngineEditor/ViewModels/MainWindowViewModel.cs
+++ IrrationalEngineCore/IrrationalEngineEditor/ViewModels/MainWindowViewModel.cs
@@ -15,28 +15,52 @@ namespace IrrationalEngineEditor.ViewModels
         public string Rotate => "Rotate something";
         public string Start => "Start the engine window";
         public string SelectedItemName => "No selected item on the scene";
+        private int rotationValue = 0;
+
+        //Values
+        public int DoRotationX
+        {
+            get { return rotationValue; }
+            set
+            {
+                if (sceneObjects == null)
+                    InitObjects();
+                sceneObjects[2].Rotation = new Vector3(value * 0.001f, sceneObjects[2].Rotation.Y, sceneObjects[2].Rotation.Z); rotationValue = value;
+            }
+        }
+
+        public int DoRotationY
+        {
+            get { return rotationValue; }
+            set
+            {
+                if (sceneObjects == null)
+                    InitObjects();
+                sceneObjects[2].Rotation = new Vector3(sceneObjects[2].Rotation.X, value * 0.001f, sceneObjects[2].Rotation.Z); rotationValue = value;
+            }
+        }
+
+
+        private List<ISceneObject> sceneObjects;
 
         //Commands 
         public ReactiveCommand<Unit, Unit> DoRunIrrationalInstance { get; }
-        public ReactiveCommand<Unit, Unit> DoRotation { get; }
 
         public MainWindowViewModel()
         {
             DoRunIrrationalInstance = ReactiveCommand.Create(RunIrrationalInstance);
-            DoRotation = ReactiveCommand.Create(RotateSomething);
-        }       
+        }
 
         void RunIrrationalInstance()
         {
             Program.context.Run();
         }
 
-        void RotateSomething()
+        void InitObjects()
         {
             SceneManager manager = Program.context.SceneManager as SceneManager;
             Scene scene = manager.Scene as Scene;
-            List<ISceneObject> sceneObjects = scene.SceneObjects;
-            sceneObjects[2].Rotation += new Vector3(30, 30, 0);
+            sceneObjects = scene.SceneObjects;
         }
     }
 }
