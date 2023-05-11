b2e0e4226dd98ee8ca57dc00656322d0a3c5676e
Serhii <kpbillia219@gmail.com>
2023-05-11 23:31:07 +0100

    Added missing files

---
+++ IrrationalEngine/IeCore/AssetManagers/ModelImporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IeCoreEntities;
+using IeCoreInterfaces.AssetImporters;
+
+namespace IeCore.AssetManagers;
+
+public class ModelImporter: IModelImporter
+{
+	public Type AssetType { get; }
+	public string[] FileExtensions { get; }
+	public Asset Import(string file)
+	{
+		throw new NotImplementedException();
+	}
+}
